ad634fee32c827fed65b9502e8c4103728be0524
Shaik Saniya Anjum <shaiksaniyaanjum956@gmail.com>
2026-02-07 12:23:50 +0530

    Add files via upload

---
+++ Relation.java
@@ -0,0 +1,14 @@
+class Relation
+{
+    public static void main(String args[])
+    {
+        int a=15;
+        int b=10;
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+    }
+}
