a9df84a56a283a3ecc29f246aa8de88a4f2661f2
Shaik Saniya Anjum <shaiksaniyaanjum956@gmail.com>
2026-02-07 11:17:10 +0530

    Add files via upload

---
+++ Marks.java
@@ -0,0 +1,13 @@
+class Marks{
+    public static void main(String args[])
+    {
+        int  maths=99;
+        int science=98;
+        int english=88;
+        int totalmarks,average;
+        totalmarks=maths+science+english;
+        average=totalmarks/3;
+        System.out.println(totalmarks);
+        System.out.println(average);
+    }
+}
