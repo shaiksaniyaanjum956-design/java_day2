8071a083f80c6eebae79878533b966a9b4dc7c60
Shaik Saniya Anjum <shaiksaniyaanjum956@gmail.com>
2026-02-07 16:12:45 +0530

    Add files via upload

---
+++ POPCORN7.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();  
+        int remaining = X - 100;
+        int buckets = remaining / 50;
+
+        System.out.println(buckets);
+
+	}
+}
