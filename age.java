6d2d01354afa371041ff4ef98389f2c12e679fb4
Shaik Saniya Anjum <shaiksaniyaanjum956@gmail.com>
2026-02-07 11:40:54 +0530

    Add files via upload

---
+++ age.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class age {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int age = s.nextInt();
+        System.out.println("Your age is: " + age);
+        s.close();
+    }
+}
