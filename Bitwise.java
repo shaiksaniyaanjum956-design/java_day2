f7f80e0bb9cffff620cf16cf778630216e249c38
Shaik Saniya Anjum <shaiksaniyaanjum956@gmail.com>
2026-02-07 14:03:56 +0530

    Add files via upload

---
+++ Bitwise.java
@@ -0,0 +1,16 @@
+class Bitwise
+{
+    public static void main(String args[])
+    {
+        int a=5;
+        int b=3;
+        System.out.println(a & b);
+        System.out.println(a | b);
+        System.out.println(a ^ b);
+        System.out.println(~a);
+        System.out.println(~b);
+        System.out.println(a << b);
+        System.out.println(a >> b);
+        System.out.println(a >>> b);
+    }
+}
