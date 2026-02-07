c18ce7b2c50a717178947ed916ace50024cf608a
Shaik Saniya Anjum <shaiksaniyaanjum956@gmail.com>
2026-02-07 11:59:54 +0530

    Add files via upload

---
+++ Arthimetic.java
@@ -0,0 +1,13 @@
+class Arthimetic
+{
+    public static void main(String args[]){
+        int a,b;
+        a=20;
+        b=10;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
